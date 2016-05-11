c74aa9a0f674ddd2eefbb8ea6d56f504c29d1d1b
Lai Wei <royweilai@gmail.com>
2016-05-11 03:45:02 -0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:roywei/RailsBlog.git'
 set :branch, :master
 set :deploy_to, '/home/deploy/RailsBlog'
 set :pty, true
-set :linked_files, %w{config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
